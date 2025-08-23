18a9039b566486f34ceb77f87ceefaff87c5941b
laura853 <lauralopess2006@gmail.com>
2025-08-22 23:28:40 -0300

    Iniciando tratamento de exceções

---
+++ src/br/com/fiap/view/Exemplo01.java
@@ -0,0 +1,33 @@
+package br.com.fiap.view;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exemplo01 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+    try {
+        //Ler dois números inteiros
+        System.out.println("Digite o número 1");
+        int n1 = leitor.nextInt();
+
+        System.out.println("Digite o número 2");
+        int n2 = leitor.nextInt();
+
+        //Dividir os números
+
+        int divisao = n1/n2;
+
+        //Exibir o resultado
+
+        System.out.println("Resultado: " + divisao);
+    } catch (InputMismatchException e){
+        System.err.println("Insira um número válido");
+    } catch (ArithmeticException e){
+        System.err.println("Não é possível realizar a divisão por zero");
+    }
+
+
+    }
+}
